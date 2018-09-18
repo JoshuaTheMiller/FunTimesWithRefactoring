eee4e8cc0baa10f3deacfe84ffbea7a98bf1e7b2
Joshua Miller <joshdan65@gmail.com>
2018-09-17 23:53:54 -0500

    Added ServiceStackCoreApp
    * To support using .Net Standard

---
+++ Projects/ServiceStackCoreApp/ServiceStackCoreApp.ServiceInterface/MyServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ServiceStack;
+using ServiceStackCoreApp.ServiceModel;
+
+namespace ServiceStackCoreApp.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Get(Hello request)
+        {
+            if (request.Name == "ThrowNotFound")
+            {
+                throw new ResourceNotFoundException();
+            }
+
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
---
+++ Projects/ServiceStackCoreApp/ServiceStackCoreApp.ServiceInterface/ResourceNotFoundException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ServiceStackCoreApp.ServiceInterface
+{
+    /// <summary>
+    /// Because people like mapping exceptions to error codes.
+    /// </summary>
+    /// <remarks>
+    /// Catching exceptions is not the performance intensive part, it's throwing them. Just think, the stack trace needs to be captured.
+    /// Throwing exceptions for workflows where something is expected, like a search result not being found, can lead to programming by exception, which has maintainability issues.
+    /// </remarks>
+    [Serializable]
+    public class ResourceNotFoundException : Exception
+    {
+        public ResourceNotFoundException()
+        {
+        }
+
+        public ResourceNotFoundException(string message) : base(message)
+        {
+        }
+
+        public ResourceNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected ResourceNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
