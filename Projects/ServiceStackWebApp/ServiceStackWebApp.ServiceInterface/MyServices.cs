5298200ac0fc0159ad3e56d29bc74898750a0e96
Joshua Miller <joshdan65@gmail.com>
2018-09-17 21:43:34 -0500

    Added initial ServiceStack projects

---
+++ Projects/ServiceStackWebApp/ServiceStackWebApp.ServiceInterface/MyServices.cs
@@ -0,0 +1,13 @@
+﻿using ServiceStack;
+using ServiceStackWebApp.ServiceModel;
+
+namespace ServiceStackWebApp.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Get(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
